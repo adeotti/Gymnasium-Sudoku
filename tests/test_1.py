86c494e6c649db4aac8c99bcd3cd5a2bd4735cbe
Wafick <wafickachiambi@gmail.com>
2026-01-12 15:25:47 -0500

    Add new test file

---
+++ tests/test_1.py
@@ -0,0 +1,31 @@
+import gymnasium_sudoku
+import gymnasium as gym
+import time,sys
+import numpy as np
+from gymnasium_sudoku.puzzle import tb_1,ts_1
+from tqdm import tqdm
+
+env = gym.make(
+        "sudoku-v0",
+        render_mode="human",
+        horizon=150,
+        render_delay=0.01,
+        eval_mode=False
+    )
+
+env.reset()
+
+for n in range(int(6e3*5)):
+    obs,reward,done,trunc,info = env.step(env.action_space.sample())
+    env.render()
+    if done:
+        print(obs)
+        assert np.all(obs!=0)
+
+        if env.unwrapped.eval_mode:
+            assert np.array_equal(obs,ts_1)
+        elif not env.unwrapped.eval_mode:
+            assert np.array_equal(obs,env.unwrapped.solution)
+        
+        time.sleep(5) # delay the board reset
+        env.reset()
