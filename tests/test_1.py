503c9482892bd5be71c9aab0ce652750bcd8e583
Wafick <wafickachiambi@gmail.com>
2026-01-12 15:47:28 -0500

    Update

--- tests/test_1.py
+++ tests/test_1.py
@@ -10,17 +10,20 @@ env = gym.make(
         render_mode="human",
         horizon=150,
         render_delay=0.01,
-        eval_mode=False
+        eval_mode=True
     )
 
 env.reset()
-steps = int(6e3*5) 
+total_steps = int(6e3*5) 
+steps = 0
 
-for n in tqdm(range(steps),total=steps):
+for n in tqdm(range(total_steps),total=total_steps):
     obs,reward,done,trunc,info = env.step(env.action_space.sample())
+    steps+=1
     env.render()
     if done:
-        print(f"\n{obs}")
+        print(f"\n{obs} | steps : {steps}")
+        steps = 0
         assert np.all(obs!=0)
 
         if env.unwrapped.eval_mode:
@@ -29,4 +32,6 @@ for n in tqdm(range(steps),total=steps):
             assert np.array_equal(obs,env.unwrapped.solution)
         
         time.sleep(5) # delay the board reset
+        
         env.reset()
+        steps = 0
